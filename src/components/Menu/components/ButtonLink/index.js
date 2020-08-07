035fcb245d512b0445eb46754a4d28a00597cd37
brunafernandes1959 <fernandesbruna1959@hotmail.com>
2020-08-07 16:59:24 -0300

    alteração do gitignore

--- src/components/Menu/components/ButtonLink/index.js
+++ src/components/Menu/components/ButtonLink/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-function ButtonLink(props){
-  //props => { className: "" , href: "/"}
-
+function ButtonLink(props) {
+  // props => { className: "o que alguém passar", href: "/" }
   return (
-    <a href="/" className={props.className} >
+    <a href={props.href} className={props.className}>
       {props.children}
     </a>
-  )
+  );
 }
 
 export default ButtonLink;
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,18 +1,22 @@
-import React from  'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
+
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
-      <img className="Logo" src={Logo} alt="BruFlix logo"/>
-      </a>
-      <Button as="a" className="ButtonLink" href="/">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </Link>
+
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo vídeo
       </Button>
     </nav>
-  )
+  );
 }
 
 export default Menu;
